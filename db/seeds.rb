cda86c6c80975144749173993439ebd981a4db9a
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-10-08 20:43:55 +0100

    resolve conflict with cards class

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ puts "Populated the data into User table "
 
 Chair.create!(name: "Office Chair", price: 120, user_id: user1.id, image_url: "https://furniture123.co.uk/Images/FNX001_1_Supersize.jpg?v=10")
 Chair.create!(name: "Gaming Chair", price: 220, user_id: user1.id, image_url: "https://xrockergaming.com/cdn/shop/articles/Neomotion_Floor_Rocker_1400x.png?v=1664995942")
-Chair.create!(name: "Dining Chair", price: 80, user_id: user2.id, image_url: "https://m.media-amazon.com/images/I/51xiO7wj55L.jpg")
+Chair.create!(name: "Dining Chair", price: 80, user_id: user2.id, image_url: "https://www.crownfrenchfurniture.co.uk/media/catalog/product/cache/d54e223a5ba71a2640c5d6d197d11109/a/r/artisan_contemporary_dining_chair.jpg")
 Chair.create!(name: "Lounge Chair", price: 150, user_id: user2.id, image_url: "https://www.coxandcox.co.uk/media/catalog/product/s/s/ss20-1226244.png?quality=80&fit=bounds&height=800&width=800")
-Chair.create!(name: "The Iron Throne", price: 100000, user_id: user3.id, image_url: "https://w0.peakpx.com/wallpaper/25/983/HD-wallpaper-daenerys-targaryen-queen-of-the-ashes-in-the-iron-throne.jpg")
+Chair.create!(name: "The Iron Throne", price: 100000, user_id: user3.id, image_url: "https://akns-images.eonline.com/eol_images/Entire_Site/2022614/rs_1024x759-220714123658-1024-Iron_Throne_from_Game_of_Thrones.jpg?fit=around%7C1024:759&output-quality=90&crop=1024:759;center,top")
 puts "Populated the date into Chair table"
