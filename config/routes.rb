79716c3ffd4a2b22cb10fb6e17de0645287ba60c
Temitayo Odubade <temitayo19@hotmail.com>
2024-10-10 14:47:43 +0100

    new booking page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :chairs, only: [ :index, :show, :new, :create ]
-  resources :bookings, only: [ :index, :show, :new, :create ]
+  resources :chairs, only: [ :index, :show, :new, :create ] do
+    resources :bookings, only: [:new, :create ]
+  end
+  resources :bookings, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
